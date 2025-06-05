ba42dffbda07b177b6a880185bd2d8e21d358199
R Jacob <rizzy@live.com.sg>
2025-06-05 14:43:36 +0800

    install daisyui

--- backend/src/main/java/com/example/mapFileServer/tile/TileService.java
+++ backend/src/main/java/com/example/mapFileServer/tile/TileService.java
@@ -1,2 +1,14 @@
-package com.example.mapFileServer.tile;public class TileService {
+package com.example.mapFileServer.tile;
+
+import com.example.mapFileServer.geoFeature.GeoFeatureRepository;
+import com.example.mapFileServer.layer.LayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TileService {
+    private final GeoFeatureRepository featureRepository;
+    private final LayerRepository layerRepository;
+
 }
